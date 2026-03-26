7bdda7a046b5f7b10c7c760921a36c9d18a85511
Emang Kgoreletso <emang@relianceinfosystems.com>
2026-03-26 16:08:57 +0200

    Delete frontend/src directory

--- frontend/src/Types/Student.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./User";
-
-export interface Student extends User {
-  studentNumber: string;
-  gradeLevel: string;
-  className: string;
-  guardianName?: string;
-  guardianContact?: string;
-}
